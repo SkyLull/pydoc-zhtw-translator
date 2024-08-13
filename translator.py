83564deb78a847ba24f9fd6d17a64cab4a82321b
Raynor@GullU <jk881222@gmail.com>
2024-08-13 12:26:29 +0800

    feat(translator.py): updated to use openai newest structured response
    uses client.beta.completion in prompting scripts, might need a migration after it became officially released

--- translator.py
+++ translator.py
@@ -1,5 +1,10 @@
 import json
 import openai
+from pydantic import BaseModel
+
+
+class TranslationResponse(BaseModel):
+    translation: str
 
 
 class Prompter():
@@ -15,7 +20,7 @@ class Prompter():
         if key is not None:
             self.client = openai.OpenAI(api_key=key)
             try:
-                info = self.client.models.retrieve('gpt-4o')
+                info = self.client.models.retrieve('gpt-4o-2024-08-06')
                 print("Model is present.")
                 return "Connected."
             except openai.AuthenticationError:
@@ -33,18 +38,23 @@ class Prompter():
             "([@5]段落的完整原文可供參考)、用詞用字(已經在[@2]及" \
             "[@3]段落描述過)及合理的RST語法(已經在[@4]段落描述過" \
             ")的前提下將其翻譯為符合RST格式的繁體中文(臺灣)字串。" \
-            "翻譯結果必須以```程式碼區塊```的方式呈現。\n" + "```\n" + request + "\n```\n"
+            "\n" + "```\n" + request + "\n```\n"
 
 
     def translate(self, request):
         if self.client is not None:
             print("prompt sent.")
-            completion = self.client.chat.completions.create(
-                model="gpt-4o",
+            completion = self.client.beta.chat.completions.parse(
+                model="gpt-4o-2024-08-06",
                 messages=[
                     {"role": "system", "content": self.system_prompt},
                     {"role": "user", "content": self.compose_request(request)}
-                ]
+                ],
+                response_format=TranslationResponse,
             )
-            print(completion.choices[0].message.content)
-            return completion.choices[0].message.content[6:-3]
+            result = completion.choices[0].message
+            if result.refusal:
+                print(result.refusal)
+            else:
+                print(result.parsed)
+            return completion.choices[0].message.parsed.translation
